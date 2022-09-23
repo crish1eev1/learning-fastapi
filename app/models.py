ce1852ee30937a57a0bc99c9ee93daa3c99bf32b
unknown <christophe.level@hotmail.fr>
2022-09-23 14:18:57 +0200

    changing tkinter import on models.py because of an issue on heroku

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from tkinter import CASCADE
+#from tkinter import CASCADE
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import TIMESTAMP
